87242cf37e8b779a948cbc99e673753ce432098c
Yury Petrashev <yurapetr@yahoo.com>
2020-09-17 14:12:39 +0700

    Added devider layout

---
+++ app/src/main/java/com/example/android/sequeniafilms0109/model/Devider.java
@@ -0,0 +1,14 @@
+package com.example.android.sequeniafilms0109.model;
+
+public class Devider {
+
+    private String mName;
+
+    public Devider(String name){
+        this.mName = name;
+    }
+
+    public String getmName(){
+        return mName;
+    }
+}
